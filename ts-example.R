f4099155497433dd3df0a0c00d522ea2385c477a
dylansun <lili.sun.zju@gmail.com>
2015-01-01 14:48:41 +0800

    update

--- ts-example.R
+++ ts-example.R
@@ -1,2 +1,7 @@
+#Learn to use 'ts' object
 souvenir <- scan("http://robjhyndman.com/tsdldata/data/fancy.dat")
 class(souvenir)
+# start: start time
+# frequency: sample frequency per year 
+souvenirtimeseries <- ts(souvenir, frequency = 12, start = c(1987,1))
+souvenirtimeseries
